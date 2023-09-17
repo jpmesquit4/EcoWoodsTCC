b9cf571afebb89623c91fd5984460917fc83b138
jpmesquit4 <joaopedromesq123@gmail.com>
2023-09-17 01:43:59 -0300

    base da api pronta

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { con } from './repository/connection.js'
+
+import express from 'express'
+import cors from 'cors'
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+
+server.listen(process.env.PORT, () => console.log(`API conectada na porta ${process.env.PORT}`))
