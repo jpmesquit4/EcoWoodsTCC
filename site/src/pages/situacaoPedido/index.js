e8d6ae89fbf508cc429ead52f3cb871c1ec345ae
Kauan <kauanpedropedrocauan@gmail.com>
2023-11-10 11:01:01 -0300

    terminando

--- site/src/pages/situacaoPedido/index.js
+++ site/src/pages/situacaoPedido/index.js
@@ -15,30 +15,95 @@ export default function Situacao() {
 
                 <div className="sub-container">
                     <h1>
-                        DETALHES DO PEDIDO
+                        DETALHES DO PEDIDO #213127R3ASB
                     </h1>
 
-                    <div className="nav-esquerda">
-                        <div className="cabecalho">
-                            <img src="/assets/images/verificar.png" alt="" />
-                            <div>
-                                <p>
-                                    PEDIDO SENDO PREPARADO
-                                    <span>
+                    <div className="sub">
+                        <div className="nav-esquerda">
+                            <div className="cabecalho">
+                                <img src="/assets/images/verificar.png" alt="" />
+                                <div>
+                                    <p className="status">
+                                        PEDIDO SENDO PREPARADO <br />
+                                    </p>
+
+                                    <p className="aguarde">
                                         AQUARDE NOVA MENSAGEM
-                                    </span>
-                                </p>
+                                    </p>
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div className="Destino">
+                                <img src="/assets/images/entrega-rapida.png" alt="" />
+                                <div>
+                                    <p className="dest">Destino:</p>
+                                    <p className="endereco">Av. Coronel Octaviano de Freitas Costa, 463 -  São Paulo - SP, 04773-000</p>
+                                </div>
                             </div>
                         </div>
+                        <div className="nav-direita">
+                            <div className="pedido">
+                                <div className="fundo-img">
+                                    <img src="" alt="" />
+                                </div>
+                                <p>
+                                    Escrivaninha de Carvalho
+                                    {/* Aqui vai ficar o nome do produto*/}
+                                    <span>1x Unidade {/* Aqui vai ficar a Quantidade */}</span>
+                                </p>
+                            </div>
 
-                        <hr />
+                            <div className="total-pedido">
+                                <span>
+                                    <p>Subtotal</p>
+                                    <span>R$325,99{/*Valor do Subtotal*/}</span>
+                                </span>
 
-                        <div className="Destino">
-                            <img src="/assets/images/entrega-rapida.png" alt="" />
+                                <span>
+                                    <p>Frete</p>
+                                    <span>R$9,90{/*Valor do Frete*/}</span>
+                                </span>
+
+                                <span>
+                                    <p>Total</p>
+                                    <span>R$335,89{/*Total do Item*/}</span>
+                                </span>
+                            </div>
+
+                            <hr />
+
+                            <div className="informações-pedido">
+                                <h2>informações do Pedido</h2>
+
+                                <span className="span-info">
+                                    <p>Endereço de Entrega</p>
+                                    <span>Av. Coronel Octaviano de Freitas Costa, 463 - São Paulo - SP, 04773-000 {/*Endereço de Entrega do Cliente*/}</span>
+                                </span>
+                                <span className="span-info">
+                                    <p>Contato</p>
+                                    <span>E-mail: kauanpedropedrocauan@gmail.com{/*Email do Cliente*/}</span>
+                                </span>
+                                <span className="span-info">
+                                    <p>Telefone</p>
+                                    <span>(11) 98760-4376{/*Numero do Cliente*/}</span>
+                                </span>
+                                <span className="span-info">
+                                    <p>Transportadora</p>
+                                    <span>SEDEX{/*Transportadora Escolhida*/}</span>
+                                </span>
+
+                                <div className="agradecimento">
+                                    <div>
+                                        <img src="/assets/images/group-name.png" alt="" />
+                                    </div>
+                                    <div className="agradecimentos-frase">
+                                        <p>Obrigado por comprar conosco, cuidaremos  de sua compra até a chegada de sua casa!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="nav-direita">
-
                     </div>
                 </div>
             </section>
