1cbb57666c439d978a82b0176f83c70380dbbf60
jpmesquit4 <you@example.com>
2023-11-22 08:56:34 -0300

    port

--- site/src/api/usuarioApi.js
+++ site/src/api/usuarioApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:6969'
+  baseURL: 'http://129.148.42.252:5020'
 });
 
 export async function login(email, senha) {
--- site/src/pages/favoritos/index.js
+++ site/src/pages/favoritos/index.js
@@ -24,7 +24,7 @@ export default function Favoritos() {
     }, [])
     
     async function filtrar() {
-        const url = `http://129.148.42.252:3020/produto/busca?nome=${filtro}`;
+        const url = `http://129.148.42.252:5020/produto/busca?nome=${filtro}`;
 
         let response = await axios.get(url);
 
@@ -54,7 +54,7 @@ export default function Favoritos() {
     }
 
     async function carregarProduto() {
-        const url = 'http://129.148.42.252:3020/produto';
+        const url = 'http://129.148.42.252:5020/produto';
 
         let response = await axios.get(url);
 
